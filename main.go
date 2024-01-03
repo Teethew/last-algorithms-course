82ac955ad5c8e6b961d9a94e458a0fea444a64b7
Teethew <thiagjack@gmail.com>
2024-01-02 22:57:14 -0300

    formatted main.go

--- main.go
+++ main.go
@@ -18,43 +18,43 @@ func main() {
 	lista.Add(9)
 
 	fmt.Printf("%s\n", lista.ToString()) // [ 3 6 9 ]
-	
+
 	listaLigada.Prepend(0)
-	
+
 	fmt.Printf("%s\n", lista.ToString()) // [ 0 3 6 9 ]
-	
+
 	result, err := lista.Get(3)
-	
+
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	
+
 	fmt.Printf("%d\n", result) // 9
-	
+
 	err = lista.Update(3, 12)
-	
+
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	
+
 	fmt.Printf("%s\n", lista.ToString()) // [ 0 3 6 12 ]
-	
+
 	err = lista.DeleteAt(3)
 
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	fmt.Printf("%s\n", lista.ToString()) // [ 0 3 6 ]	
-	
+	fmt.Printf("%s\n", lista.ToString()) // [ 0 3 6 ]
+
 	err = lista.DeleteAt(1)
 
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	fmt.Printf("%s\n", lista.ToString()) // [ 0 6 ]	
-	
+	fmt.Printf("%s\n", lista.ToString()) // [ 0 6 ]
+
 	err = lista.AddAt(1, 3)
 
 	if err != nil {
@@ -62,7 +62,7 @@ func main() {
 	}
 
 	fmt.Printf("%s\n", lista.ToString()) // [ 0 3 6 ]
-	
+
 	err = listaLigada.Remove(listaLigada.Head())
 
 	if err != nil {
